0ac8de736921b5df8d09d46c3a1baf4185914adb
Yokachi <109035246+Yokachi@users.noreply.github.com>
2022-07-12 14:45:22 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+const music = require('./index');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+
+const { Client } = require('discord.js');
+
+const client = new Client({ 
+    intents: 98303,
+});
+
+client.login('TOKEN');
+
+music.setClient(client)
+
+client.on('interactionCreate', async interaction => {
+    if(interaction.isCommand()){
+        return interaction.reply(await music.func.play('bigflo et oli',interaction.member,interaction.channel))
+    }
+})
+
+setTimeout(async x =>{
+    return console.log(await music.func.queue(client.guilds.cache.get('965555518963859537').members.cache.get('967746293302702100')))
+    
+},10000)
